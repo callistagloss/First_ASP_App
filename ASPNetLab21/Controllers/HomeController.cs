10eb45c96d7cb84dd978d82b9452c2e43b3b8322
callistagloss <49454796+callistagloss@users.noreply.github.com>
2019-05-21 22:01:36 -0400

    First ASP App

---
+++ ASPNetLab21/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNetLab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        public ActionResult Registration()
+        {
+            ViewBag.Message = "Your information.";
+
+            return View();
+    
+        }
+        public ActionResult Welcome(FormModel user)
+        {
+            
+            ViewBag.FirstName = user.FirstName;
+            ViewBag.Email = user.Email;
+            ViewBag.Password = user.Password;
+            
+            if (user.Cat == "Cat")
+            {
+                ViewBag.Cat = "You have a cat. Purrfect!";
+            }
+            else
+            {
+                ViewBag.Cat = "You don't have a cat! Meow's the time to get one.";
+            }
+            if (user.Dog == "Dog")
+            {
+                ViewBag.Dog = "You have a dog. Woof!";
+            }
+            else
+            {
+                ViewBag.Dog = "You don't have a dog! That bites.";
+            }
+            if (user.Bantha == "Bantha")
+            {
+                ViewBag.Bantha = "You have a bantha. The force is strong with you!";
+            }
+            else
+            {
+                ViewBag.Bantha = "You don't have a bantha. There might be one on eBay.";
+            }
+
+
+            return View();
+
+            
+
+        }
+
+    }
+}
---
+++ ASPNetLab21/Models/FormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNetLab21.Controllers
+{
+    public class FormModel
+    {
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Cat { get; set; }
+        public string Dog { get; set; }
+        public string Bantha { get; set; }
+    }
+}
